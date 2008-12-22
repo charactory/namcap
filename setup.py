6daf68d5f703399da628e2653f3c9d621d2cfd9d
Colin Gan <beshimi@hotmail.com>
2008-12-22 16:12:29 +0900

    grr

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-DATAFILES = [('/usr/share/man/man1', ['namcap.1'])]
+DATAFILES = [('/usr/share/man/man1', ['namcapp.1'])]
 
 setup(name="namcap",
 	version="2.0",
 	description="Pacman package analyzer",
 	author="Jason Chu",
 	author_email="jason@archlinux.org",
-	py_modules=["pacman"], packages=["Namcap"], scripts=["namcap.py", 'parsepkgbuild'],data_files =DATAFILES)
+	py_modules=["pacmanp"], packages=["Namcapp"], scripts=["namcapp.py", 'parsepkgbuildp'],data_files =DATAFILES)
 
 # vim: set ts=4 sw=4 noet:
